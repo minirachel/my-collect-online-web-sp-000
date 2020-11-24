333e5f0fa9723d9dde82a7e1710f1b6f8ed74fbd
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-24 00:37:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,12 @@
-def my_collect(array)
-  i = 0
-  list = []
-  while i < array.length
-    list << yield(array[i])
-    i += 1
-  end
-  list
-end
+# def my_collect(array)
+#   i = 0
+#   list = []
+#   while i < array.length
+#     list << yield(array[i])
+#     i += 1
+#   end
+#   list
+# end
 
 my_collect(array) do |i|
   if i.include?(" ")
