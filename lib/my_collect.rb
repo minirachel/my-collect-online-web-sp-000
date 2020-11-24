5d6ac6ad4182e087ccfc28a6e3116dce3273f657
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-24 00:40:40 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(array)
+  i = 0
+  list = []
+  while i < array.length
+    list << yield(array[i])
+    i += 1
+  end
+  list
+end
 
-
+# my_collect(array) do |i|
+#   if i.include?(" ")
+#     i.split(" ").first
+#   else
+#     i.upcase
+#   end
+# end
